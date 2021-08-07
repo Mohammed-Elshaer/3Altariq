77a6057268e92cf5c37309cfb0e1b2c61d75598e
User Name <maelshaer83@gmail.com>
2021-08-07 17:17:40 +0200

    add vedio and edite footer in mobile and add input serch in allRustrant

--- js/main.js
+++ js/main.js
@@ -37,3 +37,28 @@ $(selector).on('click', function(){
     $(selector).removeClass('active');
     $(this).addClass('active');
 });
+
+
+
+//input serch
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("filter");
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("my-candidates");
+    
+    li = document.getElementsByClassName("candidates-titel");
+  // console.log (li)
+    for (i = 0; i < li.length; i++) {
+      
+      p = li[i].getElementsByTagName("p")[0];
+        txtValue = p.textContent || p.innerText;
+        
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            ul[i].style.display = "";
+        } else {
+
+           ul[i].style.display = "none";
+        }
+    }
+  }
